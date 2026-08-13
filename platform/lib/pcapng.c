14e1d9ed015e88a1fba3912c6474a8b1e770f18c
Dev <devnaga@tutamail.com>
2026-08-13 11:33:42 +0530

    pcapng organize the structures
    Signed-off-by: Dev <devnaga@tutamail.com>

--- platform/lib/pcapng.c
+++ platform/lib/pcapng.c
@@ -52,6 +52,60 @@ typedef struct __attribute__ ((__packed__)) {
     uint64_t section_len;
 } netos_pcapng_shb_t;
 
+typedef struct {
+    char    *ifname;
+    char    *ifdesc;
+    uint8_t ts_resol;
+    char    *iffilter;
+    char    *os;
+    uint8_t fcs_len;
+} netos_pcapng_idb_opt_t;
+
+typedef struct {
+    uint16_t                link_type;
+    uint32_t                snaplen;
+    netos_pcapng_idb_opt_t  idb_opt;
+} netos_pcapng_idb_t;
+
+typedef struct {
+    uint32_t secrets_type;
+    uint32_t secrets_length;
+    uint8_t *secrets_data;
+} netos_pcapng_dsb_t;
+
+typedef struct {
+    uint16_t rec_type;
+    uint16_t rec_len;
+    uint8_t *rec;
+} netos_pcapng_nrb_t;
+
+typedef struct {
+    uint32_t    intf_id;
+    uint32_t    ts_high;
+    uint32_t    ts_low;
+    uint32_t    captured_len;
+    uint32_t    original_len;
+    uint8_t     *pkt_data;
+} netos_pcapng_epb_t;
+
+typedef struct {
+    uint32_t    original_len;
+    uint8_t     *packet_data;
+} netos_pcapng_spb_t;
+
+typedef struct {
+    bool                big_endian;
+    char                *comment;
+    char                *hw;
+    char                *os;
+    char                *app;
+    netos_pcapng_idb_t  idb;
+    netos_pcapng_epb_t  epb;
+    netos_pcapng_dsb_t  dsb;
+    netos_pcapng_nrb_t  nrb;
+    netos_pcapng_spb_t  spb;
+} netos_pcapng_file_record_t;
+
 /**
  * @brief - Defines pcapng context.
  */
